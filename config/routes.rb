5245c24a11db96de3a9e8172795f832f283080e8
janaikerana <janaikerana@gmail.com>
2022-08-29 01:11:45 +0100

    change de root fot authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
 
   resources :docs
-  
+  authenticated :user do
+    root "docs#index", as: "autenticated_root"
+  end
+
+  root 'welcome#index'
 end
